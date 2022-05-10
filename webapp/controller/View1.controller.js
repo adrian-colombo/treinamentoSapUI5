2d0cebbbd0d5a4f3879937bd2f7125859b574750
Adrian Colombo <adrian.colombo@accenture.com>
2022-05-10 20:24:19 +0000

    add msg box

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("sap.prj2.projecthelloworld2.controller.View1", {
             onInit: function () {
 
+            },
+
+            onPress: function (){
+                var vi18n = this.getView().getModel("i18n").getResourceBundle();
+                var texto = vi18n.getText("description") + ' ' + this.getView().byId("productInput").getValue();
+                MessageBox.success(texto);
             }
         });
     });
